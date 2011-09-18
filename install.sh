6547aeb6c189389525f35786375f5fe8410a6700
Mark Steve Samson <contact@marksteve.me>
2011-09-18 10:29:36 +0800

    Updated install script.

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #/bin/bash
+E='\033[1;30m[exists]'
+I='\033[1;32m[installed]'
+R='\033[0m'
 for F in bashrc bash_profile exports aliases functions vim; do
-  [ -e "`pwd`/$FF" ] && [ ! -e "$HOME/.$F" ] && ln -s `pwd`/$F $HOME/.$F && echo Linked $F
+  [ -e "`pwd`/$F" ] && ([ ! -e "$HOME/.$F" ] && ln -s `pwd`/$F $HOME/.$F && echo -e .$F $I$R) || echo -e .$F $E$R
 done
+IV=`pwd`/vim/vimrc
+EV=$HOME/.vimrc
+[ -e "$IV" ] && ([ ! -e "$EV" ] && ln -s $IV $EV && echo -e .vimrc $I$R) || echo -e .vimrc $E$R
