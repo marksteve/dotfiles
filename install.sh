b8b9a04f3b8699eab6ed81ccd94f303f92d0cf92
Mark Steve Samson <marksteve@insynchq.com>
2012-03-05 15:57:48 +0800

    Fixed install script.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ for F in bashrc bash_profile exports aliases functions vim; do
   [ -e "`pwd`/$F" ] && ([ ! -e "$HOME/.$F" ] && ln -s `pwd`/$F $HOME/.$F && echo -e .$F $I$R) || echo -e .$F $E$R
 done
 
-for F in vimrc gvimrc functions vim; do
+for F in vimrc gvimrc; do
   [ -e "`pwd`/vim/$F" ] && ([ ! -e "$HOME/.$F" ] && ln -s `pwd`/vim/$F $HOME/.$F && echo -e .$F $I$R) || echo -e .$F $E$R
 done
 
