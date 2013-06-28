8fcab4139cc4cb881b1ab07be41085029732eaa2
Mark Steve Samson <hello@marksteve.com>
2013-06-28 09:41:46 +0800

    Added linux.

--- install.sh
+++ install.sh
@@ -4,28 +4,30 @@ E='\033[1;30m[exists]'
 I='\033[1;32m[installed]'
 R='\033[0m'
 
-for F in bashrc bash_profile exports aliases functions vim tmux.conf; do
-  if [ -e "$HOME/.$F" ]; then
-    echo -e .$F $E$R
+install () {
+  if [ -e "$HOME/$2" ]; then
+    echo -e $2 $E$R
     read -p "Replace existing? (y/n) " -n 1
     echo
   fi
-  if ([ ! -e "$HOME/.$F" ] || [[ $REPLY =~ ^[Yy]$ ]]); then
-    rm $HOME/.$F > /dev/null 2>&1
-    ln -s `pwd`/$F $HOME/.$F && echo -e .$F $I$R
+  if ([ ! -e "$HOME/$2" ] || [[ $REPLY =~ ^[Yy]$ ]]); then
+    rm -r $HOME/$2 > /dev/null 2>&1
+    ln -s `pwd`/$1 $HOME/$2 && echo -e $2 $I$R
   fi
   echo
+}
+
+if [[ `uname` == 'Linux' ]]
+then
+  OS='linux'
+else
+  OS='osx'
+fi
+
+for F in bashrc bash_profile exports aliases functions; do
+  install $OS/$F .$F
 done
 
-for F in vimrc gvimrc; do
-  if [ -e "$HOME/.$F" ]; then
-    echo -e .$F $E$R
-    read -p "Replace existing? (y/n) " -n 1
-    echo
-  fi
-  if ([ ! -e "$HOME/.$F" ] || [[ $REPLY =~ ^[Yy]$ ]]); then
-    rm $HOME/.$F > /dev/null
-    ln -s `pwd`/vim/$F $HOME/.$F && echo -e .$F $I$R
-  fi
-  echo
-done
+install vim .vim
+install vim/vimrc .vimrc
+install vim/gvimrc .gvimrc
