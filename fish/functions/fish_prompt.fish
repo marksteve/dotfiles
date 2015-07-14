a850107d9b28baa5923cffb46c32b4483d86530c
Mark Steve Samson <hello@marksteve.com>
2015-07-14 17:07:52 +0800

    Removed tabs from fish functions.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-	
+
   # virtualfish
   if set -q VIRTUAL_ENV
     echo -n -s (set_color 867FFE) (basename "$VIRTUAL_ENV") " "
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,29 +1,29 @@
 function fish_right_prompt
-	
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
 
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
 
-	switch $USER
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+  end
 
-		case root
+  switch $USER
 
-		echo -n -s \
+    case root
+
+    echo -n -s \
       (set_color red) "$USER" @ "$__fish_prompt_hostname" " " \
       (prompt_pwd) \
       "$__fish_prompt_normal"
 
-		case '*'
+    case '*'
 
-		echo -n -s \
+    echo -n -s \
       (set_color 1E90FF) "$USER" @ "$__fish_prompt_hostname" " " \
       (set_color 0CFFA9) (prompt_pwd) \
       "$__fish_prompt_normal"
 
-	end
+  end
 
 end
